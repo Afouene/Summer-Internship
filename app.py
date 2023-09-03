aa87f5c3a1a7b5e44a8e768247669f5a519fd2a0
Afouene <mohamedafouene.melki@ept.ucar.tnm>
2023-09-04 01:19:15 +0200

    updates on web app

--- app.py
+++ app.py
@@ -7,14 +7,53 @@ import streamlit.components.v1 as components
 import numpy as np
 import trimesh
 from PIL import Image
+import itkwidgets as itk
 
 
 
-def killme():
-    im = Image.open("./demo/output/temp_img/temp_img.jpg")
-    mesh = trimesh.load('./demo/output/temp_img/temp_img.obj',process=False)
+def displayfunc1():
+    im=Image.open("./6/6.jpg")
+    displacement_map = Image.open("./6/6_dpmap_14.png")  # Load the displacement map
+    mesh = trimesh.load('./6/6.obj',process=False)
+
+    #im = Image.open("./demo/output/temp_img/temp_img.jpg") #the texture 
+    #displacement_map = Image.open("./demo/output/temp_img/temp_img_dpmap.png")  # Load the displacement map
+
+    #mesh = trimesh.load('./demo/output/temp_img/temp_img.obj',process=False)
     tex = trimesh.visual.TextureVisuals(image=im)
     mesh.visual.texture = tex
+    displacement_array = np.array(displacement_map)
+    print("displacement_array shape:", displacement_array.shape)
+    print("displacement_array dtype:", displacement_array.dtype)
+
+    print("mesh.vertices shape:", mesh.vertices.shape)
+    print("mesh.vertices dtype:", mesh.vertices.dtype)
+
+    
+    
+    mesh.show()
+
+def displayfunc():
+    im = Image.open("./6/6.jpg")
+    displacement_map = Image.open("./6/6_dpmap_14.png").convert("L")  # Load and convert to grayscale
+    mesh = trimesh.load('./6/6.obj', process=False)
+
+    # Convert texture to grayscale
+    im_gray = im.convert("L")
+    
+    # Convert the grayscale texture to numpy array
+    texture_array = np.array(im_gray)
+    
+    # Resize the texture array to match the displacement map
+    resized_texture_array = np.array(displacement_map.resize((mesh.vertices.shape[0], mesh.vertices.shape[1])))
+    
+    # Apply the displacement to the texture
+    displaced_texture_array = texture_array + resized_texture_array
+    
+    # Apply the displaced texture to the mesh
+    tex = trimesh.visual.TextureVisuals(image=displaced_texture_array)
+    mesh.visual.texture = tex
+
     mesh.show()
 
 
@@ -60,11 +99,11 @@ if st.button("generate 3d mesh"):
  # Check the return code to determine if the command executed successfully
     if process.returncode == 0:
         st.success("3D mesh generation successful!")
-        im = Image.open("./demo/output/temp_img/temp_img.jpg")
-        mesh = trimesh.load('./demo/output/temp_img/temp_img.obj',process=False)
-        tex = trimesh.visual.TextureVisuals(image=im)
-        mesh.visual.texture = tex
-        mesh.show()
+        st.title("3D Mesh Display")
+        displayfunc1()
+        
+
+
 
 
 
